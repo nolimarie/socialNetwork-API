7e6c6b9651d4e5d66b657e4c857817f7acf03605
agate <nolimarie@protonmail.com>
2022-01-24 23:44:58 -0700

    upload file from desktop

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# socialNetwork-API
+# Social Network API
+
+## User Story
+AS A social media startup
+I WANT an API for my social network that uses a NoSQL database
+SO THAT my website can handle large amounts of unstructured data
+## Project Description
+An API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list.
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](undefined)
+
+### Table of Contents
+* [Usage](#usage)
+* [Technologies](#tech)
+* [Installation](#installation)
+* [Demonstration](#demo)
+* [License](#license)
+* [Questions](#questions)
+
+### Usage
+Enter '''npm start''' in your terminal to start the server. From there you can test the API calls in Insomnia.
+
+### Technologies
+*node.js
+*Express
+*Mongoose
+*MongoDB
+*Insomnia
+
+### Installation
+*Clone the repository 
+*Run '''npm install''' in your terminal to aquire all dependencies
+
+### Demonstration
+Please click on link for demonstration: 
+
+### License
+MIT
+
+
+### Questions?
+Github Username: nolimarie
+
+For additional information, please reach out to me at nolimarie@msn.com
---
+++ controllers/thought-controller.js
@@ -0,0 +1,94 @@
+const { Thought, User, Reaction } = require("../models");
+
+const thoughtController = {
+    // get all Thoughts
+    getAllThought(req, res) {
+        Thought.find({})
+            .then((dbThoughtData) => res.json(dbThoughtData))
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // get one Thought by id
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .then((dbThoughtData) => {
+                // If no Thought is found, send 404
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: "No thought found with this id!" });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // createThought
+    createThought({ body }, res) {
+        Thought.create(body)
+            .then((dbThoughtData) => res.json(dbThoughtData))
+            .catch((err) => res.status(400).json(err));
+    },
+
+    // update Thought by id
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then((dbThoughtData) => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: "No thought found with this id!" });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch((err) => res.status(400).json(err));
+    },
+
+    // delete Thought
+    deleteThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.id })
+            .then((dbThoughtData) => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: "No thought found with this id!" });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch((err) => res.status(400).json(err));
+    },
+
+    // add reaction
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { replies: body } },
+            { new: true, runValidators: true }
+        )
+            .then((dbThoughtData) => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: "No thought found with this id!" });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch((err) => res.json(err));
+    },
+
+    // remove reaction
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtd },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+            .then((dbThoughtData) => res.json(dbThoughtData))
+            .catch((err) => res.json(err));
+    },
+
+};
+
+module.exports = thoughtController;
